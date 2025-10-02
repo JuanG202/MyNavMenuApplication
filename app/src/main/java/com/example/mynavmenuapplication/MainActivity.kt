5ed0af7c6bd75f5aab12d22bcc82341200c25914
granja <4684593@gmail.com>
2025-10-01 21:18:15 -0500

    actividad en clases

---
+++ app/src/main/java/com/example/mynavmenuapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.mynavmenuapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mynavmenuapplication.R.id
+import com.example.mynavmenuapplication.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                id.nav_home -> replaceFragment(HomeFragment())
+                id.nav_about -> replaceFragment(AboutFragment())
+                id.nav_services -> replaceFragment(ServicesFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+
+    }
+}
